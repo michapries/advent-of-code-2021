14f2b9e5d3b4cf8ac05da913792c69a05e3603f7
Micha-Alexander Pries <micha-alexander.pries@uni-bielefeld.de>
2021-12-02 11:51:06 +0100

    completed second challenge of day 2

--- day_2/puzzle.py
+++ day_2/puzzle.py
@@ -4,19 +4,44 @@ def first_challenge(formatted_input):
     x = 0       # Initial horizontal position
     depth = 0   # Initial depth
 
-    # Iterate over all directions and values in the input and
+    # Iterate over all commands and values in the input and
     # adjust the horizontal position and depth accordingly.
-    for direction, value in formatted_input:
+    for command, value in formatted_input:
         value = int(value)
-        if direction == 'forward':
+        if command == 'forward':
             x += value
-        elif direction == 'down':
+        elif command == 'down':
             depth += value
-        elif direction == 'up':
+        elif command == 'up':
             depth -= value
 
+    print('First challenge:')
     print('Final horizontal position:', x)
     print('Final depth:', depth)
+    print('horizontal position * depth =', x * depth)
+
+
+def second_challenge(formatted_input):
+    x = 0       # Initial horizontal position
+    depth = 0   # Initial depth
+    aim = 0     # Initial aim
+
+    # Iterate over all commands and values in the input and
+    # adjust the horizontal position, aim and depth accordingly.
+    for command, value in formatted_input:
+        value = int(value)
+        if command == 'forward':
+            x += value
+            depth += (aim * value)
+        elif command == 'down':
+            aim += value
+        elif command == 'up':
+            aim -= value
+
+    print('Second challenge:')
+    print('Final horizontal position:', x)
+    print('Final depth:', depth)
+    print('Final aim:', aim)
     print('horizontal position * depth =', x * depth)
 
 if __name__ == '__main__':
@@ -24,4 +49,8 @@ if __name__ == '__main__':
     with open('input.txt') as f:
         lines = [line.strip('\n').split() for line in f.readlines()]
 
+    print('---------------------------------------')
     first_challenge(lines)
+    print('---------------------------------------')
+    second_challenge(lines)
+    print('---------------------------------------')
