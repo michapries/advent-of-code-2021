112a2ef2a22ec463236cde376fc3a16dc7402842
Micha-Alexander Pries <micha-alexander.pries@uni-bielefeld.de>
2021-12-02 11:38:31 +0100

    completed first challenge of day 2

--- day_2/puzzle.py
+++ day_2/puzzle.py
@@ -1,8 +1,27 @@
 
 
+def first_challenge(formatted_input):
+    x = 0       # Initial horizontal position
+    depth = 0   # Initial depth
 
+    # Iterate over all directions and values in the input and
+    # adjust the horizontal position and depth accordingly.
+    for direction, value in formatted_input:
+        value = int(value)
+        if direction == 'forward':
+            x += value
+        elif direction == 'down':
+            depth += value
+        elif direction == 'up':
+            depth -= value
+
+    print('Final horizontal position:', x)
+    print('Final depth:', depth)
+    print('horizontal position * depth =', x * depth)
 
 if __name__ == '__main__':
     # Read input file
     with open('input.txt') as f:
-        lines = f.readlines()
+        lines = [line.strip('\n').split() for line in f.readlines()]
+
+    first_challenge(lines)
